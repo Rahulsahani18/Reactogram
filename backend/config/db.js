591b3c5043d4dd6798c513f20d5f222752d8793a
Rahulsahani18 <rs7613718@gmail.com>
2024-10-02 22:54:53 +0530

    Updated project with Webpack fix and latest features

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,6 @@ const colors = require('colors');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL ||"mongodb://localhost:27017/instagram", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       // Other options if needed
     });
     console.log(`Connected to MongoDB Database: ${conn.connection.host}`.bgMagenta.white);
