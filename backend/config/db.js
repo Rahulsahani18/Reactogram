36316177377bf264ddb17f20f51850fac824c3c0
Rahul sahani <153098636+Rahulsahani18@users.noreply.github.com>
2024-10-03 23:12:49 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const colors = require('colors');
-const dotenv= require('dotenv');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL {
-      // Other options if needed
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      // Add any options if needed
     });
     console.log(`Connected to MongoDB Database: ${conn.connection.host}`.bgMagenta.white);
   } catch (error) {
