b194db0c485abcf165be542b7754381c4e54b7bd
Rahul sahani <153098636+Rahulsahani18@users.noreply.github.com>
2024-10-03 23:43:48 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
+    const conn = await mongoose.connect(process.env.MONGO_URL || "mongodb+srv://rs7613718:Rahul1818@e-commerce-database.lejz8av.mongodb.net/instagram" , {
       // Add any options if needed
     });
     console.log(`Connected to MongoDB Database: ${conn.connection.host}`.bgMagenta.white);
